24fd9b38bd3594b8e8ed62d3804d694c50a85c09
OstapHetman <prickly4528@gmail.com>
2018-10-09 23:19:29 +0300

    add footer, fix some bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 $(".navbar-toggler").click(function() {
   $(this).toggleClass("open");
 });
+
+const cardBtns = document.querySelectorAll(".card .btn-link");
+
+cardBtns.forEach(cardBtn => {
+  cardBtn.addEventListener("click", e => {
+    e.target.classList.toggle("open");
+  });
+});
