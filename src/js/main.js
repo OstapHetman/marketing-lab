a18a7451084c8cd85acc843bb091f511fb2f30e1
OstapHetman <prickly4528@gmail.com>
2018-10-10 22:12:17 +0300

    implemet main modals, fix some elements

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,15 @@ $(".navbar-toggler").click(function() {
   $(this).toggleClass("open");
 });
 
-const cardBtns = document.querySelectorAll(".card .btn-link");
+if (document.querySelectorAll(".card .btn-link")) {
+  const cardBtns = document.querySelectorAll(".card .btn-link");
 
-cardBtns.forEach(cardBtn => {
-  cardBtn.addEventListener("click", e => {
-    e.target.classList.toggle("open");
+  cardBtns.forEach(cardBtn => {
+    cardBtn.addEventListener("click", e => {
+      e.target.classList.toggle("open");
+    });
   });
-});
+}
 
 setTimeout(() => {
   const preloader = document.querySelector(".preloader");
@@ -18,29 +20,31 @@ setTimeout(() => {
   body.style.overflow = "auto";
 }, 2500);
 
-const countdown = document.querySelector(".counter");
+if (document.querySelector(".counter")) {
+  const countdown = document.querySelector(".counter");
 
-const launchDate = new Date("Dec 31, 2018, 23:00:00").getTime();
+  const launchDate = new Date("Nov 1, 2018, 23:00:00").getTime();
 
-// Update every minute
-const interval = setInterval(() => {
-  // Get todays date and time (ms)
-  const now = new Date().getTime();
+  // Update every minute
+  const interval = setInterval(() => {
+    // Get todays date and time (ms)
+    const now = new Date().getTime();
 
-  // Distance from now to launch date
-  const distance = launchDate - now;
+    // Distance from now to launch date
+    const distance = launchDate - now;
 
-  // Time calculations
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display result
-  countdown.querySelector("#days h4").innerHTML = days;
-  countdown.querySelector("#hours h4").innerHTML = hours;
-  countdown.querySelector("#minutes h4").innerHTML = mins;
-  countdown.querySelector("#seconds h4").innerHTML = seconds;
-}, 1000);
+    // Display result
+    countdown.querySelector("#days h4").innerHTML = days;
+    countdown.querySelector("#hours h4").innerHTML = hours;
+    countdown.querySelector("#minutes h4").innerHTML = mins;
+    countdown.querySelector("#seconds h4").innerHTML = seconds;
+  }, 1000);
+}
