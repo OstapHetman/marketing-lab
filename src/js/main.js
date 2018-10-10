b1e767eaa4fa619ea176815934f0dba0555d77d5
OstapHetman <prickly4528@gmail.com>
2018-10-10 11:08:03 +0300

    add comming soon page

--- src/js/main.js
+++ src/js/main.js
@@ -16,4 +16,31 @@ setTimeout(() => {
 
   const body = document.getElementsByTagName("body")[0];
   body.style.overflow = "auto";
-}, 2000);
+}, 2500);
+
+const countdown = document.querySelector(".counter");
+
+const launchDate = new Date("Dec 31, 2018, 23:00:00").getTime();
+
+// Update every minute
+const interval = setInterval(() => {
+  // Get todays date and time (ms)
+  const now = new Date().getTime();
+
+  // Distance from now to launch date
+  const distance = launchDate - now;
+
+  // Time calculations
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display result
+  countdown.querySelector("#days h4").innerHTML = days;
+  countdown.querySelector("#hours h4").innerHTML = hours;
+  countdown.querySelector("#minutes h4").innerHTML = mins;
+  countdown.querySelector("#seconds h4").innerHTML = seconds;
+}, 1000);
